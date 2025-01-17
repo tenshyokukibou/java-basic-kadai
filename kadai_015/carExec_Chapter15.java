94cc5dc08db9408284503583ee262d1d624faa08
tenshyokukibou <x0l1064a@gmail.com>
2025-01-17 19:00:31 +0900

    Add files via upload

---
+++ kadai_015/carExec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_015;
+
+public class carExec_Chapter15 {
+
+	public static void main(String[] args) {
+		car_Chapter15 car = new car_Chapter15(1,10);
+
+		car.gearChange(3);
+		car.run();
+	}
+
+}
